c58dd13b2327f229269da82839ade9ef0b34996a
Ferryawan <ferry908ardiansyah@gmail.com>
2024-03-09 15:52:48 +0700

    Playlist Validaton Pipe

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpCode, Post, Res } from "@nestjs/common";
+import { Controller, Get, HttpCode, Post, Res, Header, Redirect, Param, Body, } from "@nestjs/common";
 import { response } from "express";
 import { request } from "http";
+import { CreateHeroDto } from "./dto/create-hero.dto";
  let heroes = [
       {
             id: 1,
@@ -26,9 +27,15 @@ import { request } from "http";
 export class HeroController {
       @Get("index")
       @HttpCode(200)
+      @Header('Content-Type', 'application/json')
       index(@Res() response) {
             response.json(heroes);
       }
+      @Get('detail/:id')
+      show(@Param('id') id:string){
+            return'hero by' + id;
+      }
+
 
       @Get('create')
       create(@Res({passthrough: true}) response): string {
@@ -36,16 +43,26 @@ export class HeroController {
            return 'hero create';
       }
       @Post('store')
-      store(@Res() request, @Res({passthrough: true})response) {
+      @HttpCode(201)
+      store(@Res() request, @Body() createHeroDto: CreateHeroDto , @Res({passthrough: true})response) {
             // response.status(201).json(request.body);
-            const {id, nama, type, gambar} = request.body;
-            heroes.push({
-                  id,
-                  nama,
-                  type,
-                  gambar
-            });
-
-            return heroes;
+           try{
+            // const {id, nama, type, gambar} = request.body;
+            // heroes.push({
+            //       id,
+            //       nama,
+            //       type,
+            //       gambar
+            // });
+            return createHeroDto;
+            // return heroes;
+           } catch (error) {
+            response.status(500).json({message: error});
+           }
+      }
+      @Get("/welcome")
+      @Redirect('https://docs.nestjs.com/')
+      hello() {
+            return 'welcome';
       }
 }
