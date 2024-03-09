13c526f9f5e49ae85bd091030f7821314cb24333
Ferryawan <ferry908ardiansyah@gmail.com>
2024-03-09 15:11:54 +0700

    Resource Controller

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,14 +1,51 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpCode, Post, Res } from "@nestjs/common";
+import { response } from "express";
+import { request } from "http";
+ let heroes = [
+      {
+            id: 1,
+            nama: 'Aurora',
+            type: 'mage',
+            gambar: 'aurora.jpg',
+      },
+      {
+            id: 2,
+            nama: 'Zilong',
+            type: 'Figter',
+            gambar: 'zoling.jpg',
+      },
+      {
+            id: 3,
+            nama: 'Akai',
+            type: 'Tank',
+            gambar: 'akai.jpg',
+      },
+ ]
 
 @Controller("hero")
 export class HeroController {
       @Get("index")
-      index(): string {
-            return 'hero index';
+      @HttpCode(200)
+      index(@Res() response) {
+            response.json(heroes);
       }
 
       @Get('create')
-      create() {
-            return('here create');
+      create(@Res({passthrough: true}) response): string {
+           response.cookie('name', 'baledemia');
+           return 'hero create';
+      }
+      @Post('store')
+      store(@Res() request, @Res({passthrough: true})response) {
+            // response.status(201).json(request.body);
+            const {id, nama, type, gambar} = request.body;
+            heroes.push({
+                  id,
+                  nama,
+                  type,
+                  gambar
+            });
+
+            return heroes;
       }
 }
